bc2bc747ec29bbfe52d9b685423a73ac158d28e4
CromonMS <e@cromon.co.uk>
2017-02-14 17:35:02 +0000

    Require all files?

--- lib/mscmgmt_protobuf_releases.rb
+++ lib/mscmgmt_protobuf_releases.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 require 'mscmgmt_protobuf_releases/version'
 require 'securerandom'
-require_relative 'protos/release_pb.rb'
+# require_relative 'protos/release_pb.rb'
+
+Dir[File.expand_path('lib/protos/*.rb', File.dirname(__FILE__))].each do |file|
+  require file
+end
 
 module MscmgmtProtobufReleases
   class Metadata
